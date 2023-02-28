f9d5fed3fe16bde7898f893a2a983b309147b3f4
Yash Chavan <yashchavan61099@gmail.com>
2023-02-28 22:51:12 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./App.css";
+import BarChart from "./components/BarChart";
+import { UserData } from "./Data";
+
+function App() {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+      {
+        label: "Users lost",
+        data: UserData.map((data) => data.userLost),
+        backgroundColor: [
+          "rgba(75,100,122,2)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
+
+  // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
+
+  return (
+    <div className="App">
+      <div style={{ width: 2000 }}>
+        <BarChart chartData={userData} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
